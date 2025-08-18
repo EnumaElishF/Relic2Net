e840365bfcbeda5f4c1a6b18a378a5cd18ab6497
EnumaElish <1692380369@qq.com>
2025-08-18 21:54:23 +0800

    6.3_3.3.增加热更新打包流程

--- Assets/Editor/Scripts/BuildMenuItems.cs
+++ Assets/Editor/Scripts/BuildMenuItems.cs
@@ -5,6 +5,14 @@ using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 using System.IO;
 using JKFrame;
+using HybridCLR.Editor;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Logical;
+using System.Data.SqlTypes;
+using UnityFS;
+using HybridCLR.Editor.Commands;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Build;
 /// <summary>
 /// 工具
 /// </summary>
@@ -20,7 +28,7 @@ public static class BuildMenuItems
         //因为在打包过程中会切换平台。
         //如果服务端是最后，那么在BuildSetting还要把Build做一个切换，从Dedicated Server改成Windows等。否则会出问题一直在服务端。
         Server();
-        Client();
+        NewClient();
     }
     [MenuItem("Project/Build/Server")]
     public static void Server()
@@ -57,10 +65,15 @@ public static class BuildMenuItems
         Debug.Log("完成构建服务端");
 
     }
-    [MenuItem("Project/Build/Client")]
-    public static void Client()
+    [MenuItem("Project/Build/NewClient")]
+    public static void NewClient()
     {
         Debug.Log("开始构建客户端");
+
+        //HybridCLR构建准备
+        PrebuildCommand.GenerateAll();//进行华佗的GenerateAll
+        GenerateDllBytesFile();//搬运dll文本文件
+
         List<string> sceneList = new List<string>(EditorSceneManager.sceneCountInBuildSettings);
         for (int i = 0; i < EditorSceneManager.sceneCountInBuildSettings; i++)
         {
@@ -80,9 +93,60 @@ public static class BuildMenuItems
             locationPathName = $"{projectRootPath}/{rootFloderPath}/{clientFloderPath}/Client.exe"
         };
         BuildPipeline.BuildPlayer(buildPlayerOptions);
+        //Addressables会自动构建
         Debug.Log("完成构建客户端");
     }
 
+    [MenuItem("Project/Build/UpdateClient")]
+    public static void UpdateClient()
+    {
+        //UpdateClient是不需要像NewClient那样去重新产生exe的；但是需要把资源去重新走一遍
+        Debug.Log("开始构建客户端更新包");
+        //华佗不需要去更新所有，但是需要更新dll
+        CompileDllCommand.CompileDllActiveBuildTarget();
+        GenerateDllBytesFile();
+        //Addressables更新包
+        string path = ContentUpdateScript.GetContentStateDataPath(false);
+        Debug.Log(path);
+        AddressableAssetSettings addressableAssetSettings = AddressableAssetSettingsDefaultObject.Settings;
+        //构建内容的更新: 模拟点击Update a Previous Build
+        ContentUpdateScript.BuildContentUpdate(addressableAssetSettings, path);
+
+        Debug.Log("完成构建客户端更新包");
+
+
+    }
+
+
+    [MenuItem("Project/Build/GenerateDllBytesFile")]
+    public static void GenerateDllBytesFile()
+    {
+        Debug.Log("开始生成dll文本文件");
+
+        string dllDirPath = System.Environment.CurrentDirectory + "\\" + SettingsUtil.GetHotUpdateDllsOutputDirByTarget(EditorUserBuildSettings.activeBuildTarget).Replace('/','\\');
+        string aotDllDirPath = System.Environment.CurrentDirectory  + "\\Assets\\Scripts\\DllBytes\\Aot";
+        string hotUpdateDllDirPath = System.Environment.CurrentDirectory  + "\\Assets\\Scripts\\DllBytes\\HotUpdate";
+
+        //进行复制
+        foreach(var dllName in SettingsUtil.AOTAssemblyNames)
+        {
+            //AOT路径  后缀加true表示覆盖原已有文件
+            File.Copy($"{dllDirPath}\\{dllName}.dll", $"{aotDllDirPath}\\{dllName}.dll.bytes",true);
+        }
+        //拿到不包含预留的程序集，这就是需要热更的
+        foreach(var dllName in SettingsUtil.HotUpdateAssemblyNamesExcludePreserved)
+        {
+            //热更路径下
+            File.Copy($"{dllDirPath}\\{dllName}.dll", $"{hotUpdateDllDirPath}\\{dllName}.dll.bytes", true);
+        }
+
+        //刷新文件夹
+        AssetDatabase.Refresh();
+
+        Debug.Log("完成生成dll文本文件");
+
+    }
+    #region 服务端测试宏
     public static bool editorServerTest;
     public const string editorServerTestSymbolString = "SERVER_EDITOR_TEST";
     [MenuItem("Project/TestServer")]
@@ -94,5 +158,6 @@ public static class BuildMenuItems
         else JKFrameSetting.RemoveScriptCompilationSymbol(editorServerTestSymbolString);
         Menu.SetChecked("Project/TestServer", editorServerTest);
     }
+    #endregion
 
 }
