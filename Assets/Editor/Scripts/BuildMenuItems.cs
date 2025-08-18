77f1706454df50a613040647e71424347add2e58
EnumaElish <1692380369@qq.com>
2025-08-18 12:17:29 +0800

    5.2_2.2 打包流程（一键打包客户端和服务端功能）

---
+++ Assets/Editor/Scripts/BuildMenuItems.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using System.IO;
+using JKFrame;
+
+public static class BuildMenuItems
+{
+    public const string rootFloderPath = "Builds";
+    public const string serverFloderPath = "Server";
+    public const string clientFloderPath = "Client";
+    [MenuItem("Project/Build/All")]
+    public static void All()
+    {
+        //点击打包全部的时候，要先打包服务端Server，再打包客户端
+        //因为在打包过程中会切换平台。
+        //如果服务端是最后，那么在BuildSetting还要把Build做一个切换，从Dedicated Server改成Windows等。否则会出问题一直在服务端。
+        Server();
+        Client();
+    }
+    [MenuItem("Project/Build/Server")]
+    public static void Server()
+    {
+
+        Debug.Log("开始构建服务端");
+        List<string> sceneList = new List<string>(EditorSceneManager.sceneCountInBuildSettings);
+        for (int i =  0;i < EditorSceneManager.sceneCountInBuildSettings;i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            if (scenePath != null && !scenePath.Contains("Client")) //排除Client
+            {
+                Debug.Log("添加场景:" + scenePath);
+                sceneList.Add(scenePath);
+            }
+        }
+        string projectRootPath = new DirectoryInfo(Application.dataPath).Parent.FullName;
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions()
+        {
+            scenes = sceneList.ToArray(),
+            target = BuildTarget.StandaloneWindows,
+            subtarget = (int)StandaloneBuildSubtarget.Server,
+            locationPathName = $"{projectRootPath}/{rootFloderPath}/{serverFloderPath}/Server.exe"
+        };
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        Debug.Log("完成构建服务端");
+
+    }
+    [MenuItem("Project/Build/Client")]
+    public static void Client()
+    {
+        Debug.Log("开始构建客户端");
+        List<string> sceneList = new List<string>(EditorSceneManager.sceneCountInBuildSettings);
+        for (int i = 0; i < EditorSceneManager.sceneCountInBuildSettings; i++)
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+            if (scenePath != null && !scenePath.Contains("Server")) //排除Server
+            {
+                Debug.Log("添加场景:" + scenePath);
+                sceneList.Add(scenePath);
+            }
+        }
+        string projectRootPath = new DirectoryInfo(Application.dataPath).Parent.FullName;
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions()
+        {
+            scenes = sceneList.ToArray(),
+            target = BuildTarget.StandaloneWindows,
+            subtarget = (int)StandaloneBuildSubtarget.Player,
+            locationPathName = $"{projectRootPath}/{rootFloderPath}/{clientFloderPath}/Client.exe"
+        };
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        Debug.Log("完成构建客户端");
+    }
+
+    public static bool editorServerTest;
+    public const string editorServerTestSymbolString = "SERVER_EDITOR_TEST";
+    [MenuItem("Project/TestServer")]
+    public static void TestServer()
+    {
+        editorServerTest = !editorServerTest;
+        //增加预处理指令   自定义的SERVER_EDITOR_TEST，移除预处理指令  SERVER_EDITOR_TEST
+        if (editorServerTest) JKFrameSetting.AddScriptCompilationSymbol(editorServerTestSymbolString);
+        else JKFrameSetting.RemoveScriptCompilationSymbol(editorServerTestSymbolString);
+        Menu.SetChecked("Project/TestServer", editorServerTest);
+    }
+
+}
