06903312e9837a2a1be01925b5b5957d12aa531c
EnumaElish <1692380369@qq.com>
2025-09-20 18:49:24 +0800

    18.3 导表前准备工作，大剑约束绑定修改根Root位置修改,道具Icon

---
+++ Assets/Editor/Scripts/ItemConfigImport.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class ItemConfigImport
+{
+    [MenuItem("Project/导入物品表格", priority = 1)]
+    public static void Import()
+    {
+        
+    }
+}
--- Assets/Scripts/Common/Item/Config/ItemConfigBase.cs
+++ Assets/Scripts/Common/Item/Config/ItemConfigBase.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
-
-public abstract class ItemConfigBase : ScriptableObject
+using JKFrame;
+using System.Collections.Generic;
+/// <summary>
+/// 继承的ConfigBase是继承奥丁可序列化的SO做的，方便后续使用字典上的序列化
+/// 因为Unity自带的序列化系统默认不支持 Dictionary<TKey, TValue> 类型的序列化
+/// </summary>
+public abstract class ItemConfigBase : ConfigBase
 {
 #if !UNITY_SERVER || UNITY_EDITOR
     public Sprite icon;
-
+    public Dictionary<LanguageType, string> nameDic = new Dictionary<LanguageType, string>();
+    public Dictionary<LanguageType, string> descriptionDic = new Dictionary<LanguageType, string>();
+    public string GetName(LanguageType languageType)
+    {
+        return nameDic[languageType];
+    }
+    public string GetDescription(LanguageType languageType)
+    {
+        return descriptionDic[languageType];
+    }
 #endif
 }
