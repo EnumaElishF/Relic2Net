3bc4f13a30ecc857c2463f2f99e9164841208d20
EnumaElish <1692380369@qq.com>
2025-11-06 14:21:15 +0800

    20.3 搭建怪物的结构

---
+++ Assets/Scripts/HotUpdate/Monster/MonsterClientController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterClientController : MonoBehaviour,IMonsterClientController
+{
+    public void OnNetworkDespawn()
+    {
+        
+    }
+
+    public void OnNetworkSpawn()
+    {
+        
+    }
+
+    public void PlaySkillHitEffect(Vector3 point)
+    {
+        
+    }
+
+    public void StartSkill(int skillIndex)
+    {
+        
+    }
+
+    public void StartSkillHit()
+    {
+        
+    }
+
+
+}
---
+++ Assets/Scripts/Server/Monster/MonsterServerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterServerController : CharacterServerControllerBase<MonsterController>
+{
+    protected override void OnInitAOI()
+    {
+        
+    }
+
+    protected override void OnRemoveAOI()
+    {
+        
+    }
+
+    protected override void OnUpdateAOI(Vector2Int newCoord)
+    {
+        
+    }
+
+
+}
